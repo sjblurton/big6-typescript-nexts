e22ae32f8f3aab9379dc3876cacee7e7ba2a2b4b
sjlurton <sblurton@hotmail.co.uk>
2023-08-12 09:13:30 +0100

    fixing eslint issues

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -1,14 +1,14 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { within, userEvent } from '@storybook/testing-library';
+import type {Meta, StoryObj} from "@storybook/react";
+import {within, userEvent} from "@storybook/testing-library";
 
-import { Page } from './Page';
+import Page from "./Page";
 
 const meta = {
-  title: 'Example/Page',
+  title: "Example/Page",
   component: Page,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 } satisfies Meta<typeof Page>;
 
@@ -19,9 +19,9 @@ export const LoggedOut: Story = {};
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const LoggedIn: Story = {
-  play: async ({ canvasElement }) => {
+  play: async ({canvasElement}) => {
     const canvas = within(canvasElement);
-    const loginButton = await canvas.getByRole('button', {
+    const loginButton = await canvas.getByRole("button", {
       name: /Log in/i,
     });
     await userEvent.click(loginButton);
